c05220bf9cfbeb2bcae71e9745738ff7141deb7e
Marwan Galal <marawangalal84@gmail.com>
2022-09-26 13:11:52 +0200

    trying to write object info in txt file

--- devel/icapeg/src/opnsense/mvc/app/controllers/OPNsense/ICAPeg/Api/SettingsController.php
+++ devel/icapeg/src/opnsense/mvc/app/controllers/OPNsense/ICAPeg/Api/SettingsController.php
@@ -70,13 +70,6 @@ class SettingsController extends ApiControllerBase
             // load model and update with provided data
             $mdlICAPeg = new ICAPeg();
             $mdlICAPeg->setNodes($this->request->getPost("icapeg"));
-            $objData = $this->request->getPost("icapeg");
-            $filePath = getcwd().DIRECTORY_SEPARATOR."note".DIRECTORY_SEPARATOR."notice.txt";
-            if (is_writable($filePath)) {
-                $fp = fopen($filePath, "w");
-                fwrite($fp, $objData);
-                fclose($fp);
-            }
 
             // perform validation
             $valMsgs = $mdlICAPeg->performValidation();
