2ab25ba3c08ac95a48703f515a641e3e5a74542e
Amany Mahmoud <100352155+amanymahmoud144@users.noreply.github.com>
2023-01-05 11:01:14 +0200

    Change Interface

--- devel/icapeg/src/opnsense/mvc/app/controllers/OPNsense/ICAPeg/GeneralController.php
+++ devel/icapeg/src/opnsense/mvc/app/controllers/OPNsense/ICAPeg/GeneralController.php
@@ -26,14 +26,14 @@
     POSSIBILITY OF SUCH DAMAGE.
 */
 
-namespace OPNsense\CICAP;
+namespace OPNsense\ICAPeg;
 
 class GeneralController extends \OPNsense\Base\IndexController
 {
     public function indexAction()
     {
         $this->view->generalForm = $this->getForm("general");
-        $this->view->antivirusForm = $this->getForm("antivirus");
-        $this->view->pick('OPNsense/CICAP/general');
+        // $this->view->antivirusForm = $this->getForm("antivirus");
+        $this->view->pick('OPNsense/ICAPeg/general');
     }
 }
