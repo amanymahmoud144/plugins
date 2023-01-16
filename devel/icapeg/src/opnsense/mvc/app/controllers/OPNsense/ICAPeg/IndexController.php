87e0fc54a0bfda90bc23618700afa14901ec0a03
Amany Mahmoud <100352155+amanymahmoud144@users.noreply.github.com>
2023-01-16 12:24:02 +0200

    Echo

--- devel/icapeg/src/opnsense/mvc/app/controllers/OPNsense/ICAPeg/IndexController.php
+++ devel/icapeg/src/opnsense/mvc/app/controllers/OPNsense/ICAPeg/IndexController.php
@@ -38,12 +38,13 @@ class IndexController extends \OPNsense\Base\IndexController
 {
     public function indexAction()
     {
-        // pick the template to serve to our users.
-        $this->view->pick('OPNsense/ICAPeg/index');
+       
         // fetch form data "general" in
         $this->view->generalForm = $this->getForm("general");
         // fetch form data "echo" in
         $this->view->echoForm = $this->getForm("echo");
+         // pick the template to serve to our users.
+         $this->view->pick('OPNsense/ICAPeg/index');
     }
     
 }
